02fc3296479504078cbeb4059f05ed836d140fd1
Alpha-Hotel <83920550+Alpha-Hotel@users.noreply.github.com>
2022-06-12 19:01:33 -0400

    Update ANG.py
    Charts added

--- ANG.py
+++ ANG.py
@@ -1,9 +1,14 @@
+from matplotlib.pyplot import pie, scatter
 import numpy as np
 import pandas as pd
 import datetime as dt
+import plotly.express as px
+from dash import Dash, dcc, html, Input, Output
+
+app = Dash(__name__)
 
 
-test_flag = False
+test_flag = True
 print('----##### LOADING DATA ####----')
 if test_flag == False:
     df = pd.read_excel(
@@ -13,10 +18,10 @@ if test_flag == False:
 else:
     df = {
         'Sheet1': pd.DataFrame({
-            '1': ['Last Inv Dt/Tm', '2020-04-29', '2020-05-29', '2022-05-29', '2022-05-29', '2021-06-29'],
-            '2': ['UIC', 'FA706', 'FA489', 'FA489', 'FA489', 'FA489'],
-            '3': ['Loc', 'TX', 'AK', 'DE', 'MI', ''],
-            '4': ['Serial Number', '1234', '45677', '45678', '098765', '965769']
+            '1': ['Last Inv Dt/Tm', '2020-04-29', '2020-05-29', '2022-05-29', '2022-05-29', '2021-06-29', '2021-06-29', '2020-04-29'],
+            '2': ['UIC', 'FA706', 'FA489', 'FA489', 'FA489', 'FA489', 'FA706', 'FA334'],
+            '3': ['Loc', 'TX', 'AK', 'DE', 'MI', '', '', 'AK'],
+            '4': ['Serial Number', '1234', '45677', '45678', '098765', '965769', '21412', '213']
         })}
     states_df = pd.DataFrame({
 
@@ -72,10 +77,11 @@ output = pd.DataFrame({
     'Unit Assets': [np.nan],
     'Completed Inspections': [np.nan],
     'Inspections Due': [np.nan],
-    'Inspection Percentage': [np.nan],
-    'Due in 6': [np.nan]
+    'Completion Percentage': [np.nan],
+    'Due in the Next 6 Monts': [np.nan],
+    'State': [np.nan],
+    'Greater Than 6': [np.nan]
 })
-
 
 for i in Unit_Names:
     print('{Unit}\nUnit Assets: \n{total}\nCompleted Inspections:\n{complete}\nUnit Inspections Due:\n{due}\nUnit Inspection Completion Percentage:\n{p}%\nUnit # Inspections Coming Due in Next 6 Months\n{sixm}\n'.format(
@@ -91,10 +97,88 @@ for i in Unit_Names:
         'Completed Inspections': [Unit_Completed_Assets[i].shape[0]],
         'Inspections Due': [Unit_Due_Inspections[i].shape[0]],
         'Inspection Percentage': [int(Unit_Percent_Complete[i])],
-        'Due in 6': [Unit_Due_In_6[i].shape[0]]
+        'Due in 6': [Unit_Due_In_6[i].shape[0]],
+        'Greater than 6':[(Unit_Completed_Assets[i].shape[0]-Unit_Due_In_6[i].shape[0])],
+        'State': [np.nan]
     })])
 
 
 output = output.reset_index(drop=True)
 output = output.dropna(how='all')
-print(output.to_csv('out.csv', index=False))
+output.to_csv('out.csv', index=False)
+
+#   Make Figures    #
+app_options = pd.concat([Unit_Names, pd.Series(['All Units'])])
+
+app.layout = html.Div([
+    html.H4('Inspection Health by Unit'),
+    dcc.Dropdown(
+        id="bardropdown",
+        options=['Grouped', 'Stacked'],
+        value="Grouped",
+        clearable=False,
+    ),
+    dcc.Dropdown(
+        id="dropdown",
+        options=app_options,
+        value=Unit_Names[0],
+        clearable=False,
+    ),
+    dcc.Graph(id="graph"),
+    html.H4('Overall Inspection Health'),
+    dcc.Graph(id="pie"),
+    html.H4("Percentage of Complete Inspections"),
+    dcc.Graph(id='scatter')
+])
+
+
+@ app.callback(
+    Output("graph", "figure"),
+    Input("dropdown", "value"),
+    Input("bardropdown", "value"))
+def update_bar_chart(Unit, graph_type):
+    if Unit == 'All Units':
+        mask = pd.Series(True, index=output['Unit'].index)
+    else:
+        mask = output['Unit'] == Unit
+
+    print(graph_type)
+    # Group Bar chart
+    if graph_type == "Grouped":
+        fig = px.bar(output[mask], x="Unit", y=[
+            'Greater than 6', 'Due in 6', 'Inspections Due'],
+            barmode="group", color_discrete_sequence=["DarkGreen", "Goldenrod", "FireBrick"], text_auto=True)
+    else:
+        fig = px.bar(output[mask], x="Unit", y=[
+            'Greater than 6', 'Due in 6', 'Inspections Due'], color_discrete_sequence=["DarkGreen", "Goldenrod", "FireBrick"], title="Long-Form Input")
+
+    return fig
+
+
+pie_values = pd.DataFrame({'values': [
+    output['Greater than 6'].sum(), output['Due in 6'].sum(),
+    output['Inspections Due'].sum()]})
+
+print(pie_values)
+
+
+@app.callback(
+    Output("pie", "figure"),
+    Input("dropdown", "value"))
+def generate_chart(Unit):
+    fig = px.pie(pie_values, values='values', names=[
+        'Greater than 6', 'Due in 6', 'Inspections Due'], color_discrete_sequence=["DarkGreen", "Goldenrod", "FireBrick"], hole=.3)
+    return fig
+
+
+@app.callback(
+    Output("scatter", "figure"),
+    Input("dropdown", "value")
+)
+def generate_scatter(value):
+    mask = pd.Series(True, index=output['Unit'].index)
+    fig = px.scatter(output[mask], x="Unit", y="Inspection Percentage")
+    return fig
+
+
+app.run_server(debug=True)
